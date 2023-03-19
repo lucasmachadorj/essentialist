733917c2f7f1281c6cf092ec7f1defe99480c6a8
Lucas Machado <lucasmachado.rj@gmail.com>
2023-03-19 15:40:34 -0300

    [feat] fizzbuzz returns a string for any input

---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,5 @@
+function fizzbuzz(n: number): string {
+  return "";
+}
+
+export { fizzbuzz };
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,8 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  it("should return a string for any number such as 1 or 3", () => {
+    expect(typeof fizzbuzz(1)).toBe("string");
+    expect(typeof fizzbuzz(3)).toBe("string");
+  });
 });
