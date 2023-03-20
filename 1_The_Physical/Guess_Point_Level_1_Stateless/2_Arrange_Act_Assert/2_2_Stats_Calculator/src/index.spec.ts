2237858d9f8a1fd1e1cf45d01e95a8bfcd3c4d75
Lucas Machado <lucasmachado.rj@gmail.com>
2023-03-20 01:01:11 -0300

    [feat] min, max, count and average functions to compose statsCalculator

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,4 +1,14 @@
-import { Stats, statsCalculator } from "./index";
+import { Stats, average, max, min, count, statsCalculator } from "./index";
+
+type Input = number[];
+type ExpectedOutput = number;
+
+const methods: Record<string, Function> = {
+  min,
+  max,
+  average,
+  count,
+};
 
 describe("stats calculator", () => {
   it("should return an object with the correct stats for a given array of numbers such as [1,2,3,4,5]", () => {
@@ -8,4 +18,28 @@ describe("stats calculator", () => {
     expect(result.average).toBe(3);
     expect(result.count).toBe(5);
   });
+
+  const testCases: [Input, keyof typeof methods, ExpectedOutput][] = [
+    [[1, 2, 3, 4, 5], "min", 1],
+    [[1, 2, 3, 4, 5], "max", 5],
+    [[1, 2, 3, 4, 5], "average", 3],
+    [[1, 2, 3, 4, 5], "count", 5],
+    [[-5, -4, -4, 0, 1, 3, 4, 4, 6, 10], "min", -5],
+    [[-5, -4, -4, 0, 1, 3, 4, 4, 6, 10], "max", 10],
+    [[-5, -4, -4, 0, 1, 3, 4, 4, 6, 10], "average", 1.5],
+    [[-5, -4, -4, 0, 1, 3, 4, 4, 6, 10], "count", 10],
+    [[-10, -100, -50, 0, 0, 0, 10, 100, 50], "min", -100],
+    [[-10, -100, -50, 0, 0, 0, 10, 100, 50], "max", 100],
+    [[-10, -100, -50, 0, 0, 0, 10, 100, 50], "average", 0],
+    [[-10, -100, -50, 0, 0, 0, 10, 100, 50], "count", 9],
+  ];
+
+  describe.each(testCases)(
+    "Given the input %s, when the method %s is invoked",
+    (input, statsMethod, expectedOutput) => {
+      it(`Then it should return ${expectedOutput}`, () => {
+        expect(methods[statsMethod](input)).toEqual(expectedOutput);
+      });
+    }
+  );
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -5,11 +5,31 @@ export type Stats = {
   count: number;
 };
 
+const minBetween = (a: number, b: number): number => (a < b ? a : b);
+const maxBetween = (a: number, b: number): number => (a > b ? a : b);
+const updateAverage = (
+  sum: number,
+  currentValue: number,
+  _: number,
+  numbers: Array<number>
+) => (sum * numbers.length + currentValue) / numbers.length;
+
+export const min = (numbers: number[]): number =>
+  numbers.reduce(minBetween, numbers[0]);
+
+export const max = (numbers: number[]): number =>
+  numbers.reduce(maxBetween, numbers[0]);
+
+export const average = (numbers: number[]): number =>
+  numbers.reduce(updateAverage, 0);
+
+export const count = (numbers: number[]): number => numbers.length;
+
 export const statsCalculator = (numbers: number[]): Stats => {
   return {
-    min: 1,
-    max: 5,
-    average: 3,
-    count: 5,
+    min: min(numbers),
+    max: max(numbers),
+    average: average(numbers),
+    count: count(numbers),
   };
 };
