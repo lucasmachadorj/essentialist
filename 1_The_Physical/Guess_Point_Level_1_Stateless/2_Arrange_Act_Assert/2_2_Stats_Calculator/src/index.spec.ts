60820cc7cd810ae1bf5d16efdf7af67009681664
Lucas Machado <lucasmachado.rj@gmail.com>
2023-03-20 00:03:29 -0300

    [feat] statsCalculator returns object with right stats for input [1,2,3,4,5]

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,11 @@
+import { statsCalculator } from "./index";
 
-describe('stats calculator', () => {
-
-
-})
+describe("stats calculator", () => {
+  it("should return an object with the correct stats for a given array of numbers such as [1,2,3,4,5]", () => {
+    const result = statsCalculator([1, 2, 3, 4, 5]);
+    expect(result.min).toBe(1);
+    expect(result.max).toBe(5);
+    expect(result.average).toBe(3);
+    expect(result.count).toBe(5);
+  });
+});
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -0,0 +1,8 @@
+export const statsCalculator = (numbers: number[]) => {
+  return {
+    min: 1,
+    max: 5,
+    average: 3,
+    count: 5,
+  };
+};
