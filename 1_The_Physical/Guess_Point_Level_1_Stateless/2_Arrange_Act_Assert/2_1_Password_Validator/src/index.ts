14ac87ee7a282aeed19ac7bbed7716327ff95a95
Lucas Machado <lucasmachado.rj@gmail.com>
2023-03-19 23:34:50 -0300

    [refactor] improve code readability providing meaningful names for validations

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,24 +1,33 @@
+const hasLessThan = (n: number) => (s: string) => s.length < n;
+const hasMoreThan = (n: number) => (s: string) => s.length > n;
+
+const hasLessThan5 = hasLessThan(5);
+const hasMoreThan15 = hasMoreThan(15);
+const hasNoUppercase = (s: string) => !s.match(/[A-Z]/);
+const hasNoDigit = (s: string) => !s.match(/\d/);
+const passwordIsValid = (errors: string[]) => errors.length === 0;
+
 export const passwordValidator = (password: string) => {
   let errors = [];
 
-  if (password.length < 5) {
+  if (hasLessThan5(password)) {
     errors.push("Password must be at least 5 characters");
   }
 
-  if (password.length > 15) {
+  if (hasMoreThan15(password)) {
     errors.push("Password must be at most 15 characters");
   }
 
-  if (!password.match(/[A-Z]/)) {
+  if (hasNoUppercase(password)) {
     errors.push("Password must contain at least one uppercase letter");
   }
 
-  if (!password.match(/\d/)) {
+  if (hasNoDigit(password)) {
     errors.push("Password must contain at least one digit");
   }
 
   return {
-    success: errors.length === 0,
+    success: passwordIsValid(errors),
     errors: errors,
   };
 };
