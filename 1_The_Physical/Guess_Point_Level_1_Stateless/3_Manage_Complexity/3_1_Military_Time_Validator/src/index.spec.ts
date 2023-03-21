0212939ab50a51ea723de69a60edaaa89cb75086
Lucas Machado <lucasmachado.rj@gmail.com>
2023-03-21 01:10:47 -0300

    [feat] it knows that an empty string is not a valid time

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("military time validator", () => {
   it("should return true when input is 00:00 - 01:00", () => {
     expect(isMilitaryTime("00:00 - 01:00")).toBe(true);
   });
+
+  it("should know an empty string is not a valid time", () => {
+    expect(isMilitaryTime("")).toBe(false);
+  });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,3 +1,4 @@
 export const isMilitaryTime = (time: string): boolean => {
+  if (time === "") return false;
   return true;
 };
