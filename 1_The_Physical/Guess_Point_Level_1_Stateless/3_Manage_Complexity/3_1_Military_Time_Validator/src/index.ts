70501982f19bc5d56c6f9d8288e7ee7839b85ba5
Lucas Machado <lucasmachado.rj@gmail.com>
2023-03-21 02:12:27 -0300

    [refactor] reduce function abstractions

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -2,10 +2,10 @@ const leftTime = (time: string): string => time.split(" - ")[0];
 const rightTime = (time: string): string => time.split(" - ")[1];
 const isNotValidTimeFormat = (time: string): boolean =>
   time.split(":").length !== 2;
-const getHour = (time: string): number => parseInt(time.split(":")[0]);
-const invalidHour = (hour: number): boolean => hour < 0 || hour > 23;
-const getMinute = (time: string): number => parseInt(time.split(":")[1]);
-const invalidMinute = (minute: number): boolean => minute < 0 || minute > 59;
+const invalidHour = (time: string): boolean =>
+  parseInt(time.split(":")[0]) < 0 || parseInt(time.split(":")[0]) > 23;
+const invalidMinute = (time: string): boolean =>
+  parseInt(time.split(":")[1]) < 0 || parseInt(time.split(":")[1]) > 59;
 
 export const isMilitaryTime = (time: string): boolean => {
   if (time === "") return false;
@@ -16,16 +16,9 @@ export const isMilitaryTime = (time: string): boolean => {
   )
     return false;
 
-  if (
-    invalidHour(getHour(leftTime(time))) ||
-    invalidHour(getHour(rightTime(time)))
-  )
-    return false;
+  if (invalidHour(leftTime(time)) || invalidHour(rightTime(time))) return false;
 
-  if (
-    invalidMinute(getMinute(leftTime(time))) ||
-    invalidMinute(getMinute(rightTime(time)))
-  )
+  if (invalidMinute(leftTime(time)) || invalidMinute(rightTime(time)))
     return false;
 
   return true;
