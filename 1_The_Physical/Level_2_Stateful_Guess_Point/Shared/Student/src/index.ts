6771ce4eaf3a2990526c2bd35a10a52e8274f4eb
Lucas Machado <lucasmachado.rj@gmail.com>
2023-04-29 18:56:56 -0300

    [refactor] extract function that validated firstName

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/index.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/index.ts
@@ -3,20 +3,22 @@ export interface StudentProps {
   lastName: string;
 }
 
+const validateFirstName = (firstName: string): void => {
+  if (firstName.length < 2)
+    throw new Error("firstName must be at least 2 characters");
+  if (firstName.length > 10)
+    throw new Error("firstName must be at most 10 characters");
+  if (/[^a-zA-Z]/.test(firstName))
+    throw new Error("firstName must contain only alphabetic characters");
+};
+
 export class Student {
   private constructor(private firstName: string, private lastName: string) {}
 
   static create(props: StudentProps): Student {
     const { firstName, lastName } = props;
-    if (firstName.length < 2)
-      throw new Error("firstName must be at least 2 characters");
 
-    if (firstName.length > 10)
-      throw new Error("firstName must be at most 10 characters");
-
-    if (/[^a-zA-Z]/.test(firstName)) {
-      throw new Error("firstName must contain only alphabetic characters");
-    }
+    validateFirstName(firstName);
 
     if (lastName.length < 2) {
       throw new Error("lastName must be at least 2 characters");
