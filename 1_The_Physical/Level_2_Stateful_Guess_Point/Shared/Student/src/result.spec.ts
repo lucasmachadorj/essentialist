5d04e4e8dfc74cc90a992ef5eb935ac2b93614be
Lucas Machado <lucasmachado.rj@gmail.com>
2023-05-01 10:57:15 -0300

    [green] should be defined

---
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/result.spec.ts
@@ -0,0 +1,9 @@
+export class Result {}
+
+describe("Error handling object", () => {
+  describe("Result", () => {
+    it("should be defined", () => {
+      expect(Result).toBeDefined();
+    });
+  });
+});
