183efe2462709cd80b195da0ae5c7e252079260d
Lucas Machado <lucasmachado.rj@gmail.com>
2023-05-01 11:15:02 -0300

    [green] should have a isOk static method that returns an instance of Result

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/result.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/result.spec.ts
@@ -1,10 +1,52 @@
-export class Result {}
+enum MaybeType {
+  Just = "Just",
+  Nothing = "Nothing",
+}
+
+interface Just<T> {
+  type: MaybeType.Just;
+  value: T;
+}
+
+interface Nothing {
+  type: MaybeType.Nothing;
+}
+
+type Maybe<T> = Just<T> | Nothing;
+
+const Just = <T>(value: T): Just<T> => ({
+  type: MaybeType.Just,
+  value,
+});
+
+const Nothing = (): Nothing => ({
+  type: MaybeType.Nothing,
+});
+
+export class Result<T, E> {
+  private value: Maybe<T>;
+  private error: Maybe<E>;
+
+  private constructor(value: T | null, error: E | null) {
+    this.value = value ? Just(value) : Nothing();
+    this.error = error ? Just(error) : Nothing();
+  }
+
+  static isOk<T>(value: T): Result<T, Nothing> {
+    return new Result<T, never>(value, null);
+  }
+}
 
 describe("Error handling object", () => {
   describe("Result", () => {
     it("should be defined", () => {
       expect(Result).toBeDefined();
       expect(Result).toBeInstanceOf(Function);
+    });
+
+    it("should have a isOk static method that returns an instance of Result", () => {
+      expect(Result.isOk).toBeDefined();
+      expect(Result.isOk(1)).toBeInstanceOf(Result);
     });
   });
 });
