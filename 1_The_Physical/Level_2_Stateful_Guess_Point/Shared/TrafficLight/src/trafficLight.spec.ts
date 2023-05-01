d6833a6c95fac9b16597d9fe7fcbc5a2f90c1a7b
Lucas Machado <lucasmachado.rj@gmail.com>
2023-05-01 19:31:20 -0300

    [green] should start off

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/trafficLight.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/trafficLight.spec.ts
@@ -1,9 +1,31 @@
+enum Status {
+  Red = "red",
+  Yellow = "yellow",
+  Green = "green",
+  Off = "off",
+  On = "on",
+}
+
+type TrafficLightProps = {
+  currentStatus: Status;
+};
+
 class TrafficLight {
+  private readonly currentStatus: Status = Status.Off;
   constructor() {}
+
+  isOff() {
+    return this.currentStatus === Status.Off;
+  }
 }
 
 describe("TrafficLight", () => {
   it("should be defined", () => {
     expect(TrafficLight).toBeDefined();
+  });
+
+  it("should start off", () => {
+    const trafficLight = new TrafficLight();
+    expect(trafficLight.isOff()).toBe(true);
   });
 });
