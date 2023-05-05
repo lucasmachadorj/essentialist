0c5bbeb0b7082cb5bf0b208749573cd37de7f7c0
Lucas Machado <lucasmachado.rj@gmail.com>
2023-05-05 17:16:20 -0300

    [green] should start with an empty board

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tictactoe.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tictactoe.spec.ts
@@ -1,9 +1,26 @@
 class Game {
-  constructor() {}
+  private board: string[][];
+
+  constructor() {
+    this.board = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+  }
+
+  isBoardEmpty(): boolean {
+    return this.board.every((row) => row.every((cell) => cell === ""));
+  }
 }
 
 describe("Tic tac toe game", () => {
   it("should be defined", () => {
     expect(Game).toBeDefined();
+  });
+
+  it("should start with an empty board", () => {
+    const game = new Game();
+    expect(game.isBoardEmpty()).toEqual(true);
   });
 });
