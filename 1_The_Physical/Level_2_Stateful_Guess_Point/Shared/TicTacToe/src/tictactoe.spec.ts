694eb658358a88283e18537ba53dfaaee75dcc76
Lucas Machado <lucasmachado.rj@gmail.com>
2023-05-05 17:29:53 -0300

    [refactor] beforeEach, instantiate game - rule of 3

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tictactoe.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tictactoe.spec.ts
@@ -47,46 +47,45 @@ class Game {
 }
 
 describe("Tic tac toe game", () => {
+  let game: Game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
   it("should be defined", () => {
     expect(Game).toBeDefined();
   });
 
   it("should start with an empty board", () => {
-    const game = new Game();
     expect(game.isBoardEmpty()).toEqual(true);
   });
 
   it("should have a board with 9 cells", () => {
-    const game = new Game();
     expect(game.boardSize).toEqual(9);
   });
 
   it("should start with the player X", () => {
-    const game = new Game();
     expect(game.currentTurn()).toEqual("X");
   });
 
   it("should mark the first cell with X when first player chooses the first cell", () => {
-    const game = new Game();
     game.playAt(0, 0);
     expect(game.playerAt(0, 0)).toEqual("X");
   });
 
   it("should be O turn after X plays", () => {
-    const game = new Game();
     game.playAt(0, 0);
     expect(game.currentTurn()).toEqual("O");
   });
 
   it("should mark the second cell with O when second player chooses the second cell", () => {
-    const game = new Game();
     game.playAt(0, 0);
     game.playAt(0, 1);
     expect(game.playerAt(0, 1)).toEqual("O");
   });
 
   it("should be X turn after O plays", () => {
-    const game = new Game();
     game.playAt(0, 0);
     game.playAt(0, 1);
     expect(game.currentTurn()).toEqual("X");
