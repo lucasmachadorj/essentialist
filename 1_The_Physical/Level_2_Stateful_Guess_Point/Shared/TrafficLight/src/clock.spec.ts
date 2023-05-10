cf92b29d48ec4300ce0f2e491e2a9846810b3c0f
Lucas Machado <lucasmachado.rj@gmail.com>
2023-05-10 00:10:47 -0300

    [green] should start with time delay of 0

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
@@ -1,7 +1,22 @@
-class Clock {}
+class Clock {
+  private timeDelay: number;
+
+  constructor() {
+    this.timeDelay = 0;
+  }
+
+  getTimeDelay(): number {
+    return this.timeDelay;
+  }
+}
 
 describe("Clock", () => {
   it("should be defined", () => {
     expect(Clock).toBeDefined();
+  });
+
+  it("should start with time delay of 0", () => {
+    const clock = new Clock();
+    expect(clock.getTimeDelay()).toBe(0);
   });
 });
