a74120695bcd1aacdbcf9682b1dded71f8b7c9dc
Lucas Machado <lucasmachado.rj@gmail.com>
2023-05-10 00:17:06 -0300

    [green] should be able to increase time delay, such as add 30 s

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
@@ -8,6 +8,10 @@ class Clock {
   getTimeDelay(): number {
     return this.timeDelay;
   }
+
+  increaseTimeDelay(timeDelay: number): void {
+    this.timeDelay += timeDelay;
+  }
 }
 
 describe("Clock", () => {
@@ -18,5 +22,11 @@ describe("Clock", () => {
   it("should start with time delay of 0", () => {
     const clock = new Clock();
     expect(clock.getTimeDelay()).toBe(0);
+  });
+
+  it("should be able to increase time delay, such as add 30 s", () => {
+    const clock = new Clock();
+    clock.increaseTimeDelay(30);
+    expect(clock.getTimeDelay()).toBe(30);
   });
 });
