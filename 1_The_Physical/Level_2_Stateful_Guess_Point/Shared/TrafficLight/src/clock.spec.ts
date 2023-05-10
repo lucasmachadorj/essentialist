ce2f56e245a6bd70d273a5b27f85bd5aad1a5d9a
Lucas Machado <lucasmachado.rj@gmail.com>
2023-05-10 00:57:14 -0300

    [refactor] move clock to its own module

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
@@ -1,30 +1,5 @@
+import { Clock } from "./clock";
 import { TrafficLight } from "./trafficLight";
-
-class Clock {
-  private timeDelay: number;
-  private subscribers: TrafficLight[];
-
-  constructor() {
-    this.timeDelay = 0;
-    this.subscribers = [];
-  }
-
-  getTimeDelay(): number {
-    return this.timeDelay;
-  }
-
-  increaseTimeDelay(timeDelay: number): void {
-    this.timeDelay += timeDelay;
-  }
-
-  subscribe(trafficLight: TrafficLight): void {
-    this.subscribers.push(trafficLight);
-  }
-
-  getSubscribers(): TrafficLight[] {
-    return this.subscribers;
-  }
-}
 
 describe("Clock", () => {
   it("should be defined", () => {
---
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.ts
@@ -0,0 +1,27 @@
+import { TrafficLight } from "./trafficLight";
+
+export class Clock {
+  private timeDelay: number;
+  private subscribers: TrafficLight[];
+
+  constructor() {
+    this.timeDelay = 0;
+    this.subscribers = [];
+  }
+
+  getTimeDelay(): number {
+    return this.timeDelay;
+  }
+
+  increaseTimeDelay(timeDelay: number): void {
+    this.timeDelay += timeDelay;
+  }
+
+  subscribe(trafficLight: TrafficLight): void {
+    this.subscribers.push(trafficLight);
+  }
+
+  getSubscribers(): TrafficLight[] {
+    return this.subscribers;
+  }
+}
