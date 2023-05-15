f3324b46248d54425b225d321cfb13cc744d4a3a
Lucas Machado <lucasmachado.rj@gmail.com>
2023-05-15 17:48:54 -0300

    [refactor] rule of three -> extract cache, controller and repo initialization

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/core/controller.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/core/controller.spec.ts
@@ -4,28 +4,27 @@ import { Repository } from "./repository";
 
 describe("Controller commands use cases", () => {
   describe("Given the user opens the traffic light page", () => {
-    it("should initialize a clock with value 0", () => {
-      const cache = new GlobalCache();
-      const repository = new Repository(cache);
-      const controller = new Controller(repository);
+    let controller: Controller;
+    let repository: Repository;
+    let cache: GlobalCache;
+
+    beforeEach(() => {
+      cache = new GlobalCache();
+      repository = new Repository(cache);
+      controller = new Controller(repository);
       controller.initializeClock();
+    });
+
+    it("should initialize a clock with value 0", () => {
       expect(repository.getCurrentTime()).toBe(0);
     });
 
     it("should update the clock time", () => {
-      const cache = new GlobalCache();
-      const repository = new Repository(cache);
-      const controller = new Controller(repository);
-      controller.initializeClock();
       controller.updateClock();
       expect(repository.getCurrentTime()).toBe(1);
     });
 
     it("should add a new traffic light", () => {
-      const cache = new GlobalCache();
-      const repository = new Repository(cache);
-      const controller = new Controller(repository);
-      controller.initializeClock();
       controller.addTrafficLight();
       expect(repository.getTrafficLights().length).toBe(1);
     });
