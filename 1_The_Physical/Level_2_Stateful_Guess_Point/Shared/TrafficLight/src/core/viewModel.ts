17aa502b1d789eb37983dfc79ad06e2b68835e4a
Lucas Machado <lucasmachado.rj@gmail.com>
2023-05-15 18:25:33 -0300

    [refactor] view model state props readonly

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/core/viewModel.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/core/viewModel.ts
@@ -1,22 +1,44 @@
-export class ViewModel {
-  private currentTime: number = -1;
-  private trafficLights: { id: string; currentState: string }[] = [];
+type TimeState = number;
 
-  constructor() {}
+type TrafficLightState = {
+  readonly id: string;
+  readonly currentState: string;
+};
+
+type ViewModelProps = {
+  readonly currentTime: TimeState;
+  readonly trafficLights: TrafficLightState[];
+};
+
+export class ViewModel {
+  private props: ViewModelProps;
+
+  constructor() {
+    this.props = {
+      currentTime: -1,
+      trafficLights: [],
+    };
+  }
 
   setCurrentTime(currentTime: number) {
-    this.currentTime = currentTime;
+    this.props = {
+      ...this.props,
+      currentTime: currentTime,
+    };
   }
 
   setTrafficLights(trafficLights: { id: string; currentState: string }[]) {
-    this.trafficLights = trafficLights;
+    this.props = {
+      ...this.props,
+      trafficLights: trafficLights,
+    };
   }
 
   getCurrentTime(): number {
-    return this.currentTime;
+    return this.props.currentTime;
   }
 
   getTrafficLights() {
-    return this.trafficLights;
+    return this.props.trafficLights;
   }
 }
